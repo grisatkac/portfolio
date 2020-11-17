b948a4a5e403c48c3430e7572252d5e34179dcb8
Grisha Tkach <grisatkac@mail.ru>
2020-11-17 10:33:24 +0300

    change main server file

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use('/api/auth', require('./src/server/routes/auth.routes'))
 
 async function start() {
     try {
-        await mongoose.connect('mongodb+srv://Grisha:123456Aa@cluster0.f4mbl.azure.mongodb.net/portfolio?retryWrites=true&w=majority', {
+        'mongodb+srv://Grisha:<password>@cluster0.zrczy.mongodb.net/<dbname>?retryWrites=true&w=majority'
+        //await mongoose.connect('mongodb+srv://Grisha:123456Aa@cluster0.f4mbl.azure.mongodb.net/portfolio?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://Grisha:123456Aa@cluster0.zrczy.mongodb.net/<dbname>?retryWrites=true&w=majority', {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true
