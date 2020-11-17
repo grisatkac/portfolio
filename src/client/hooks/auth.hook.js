5599c38ce6792f7d726d7e912668dfb0b84e42e5
Grisha Tkach <grisatkac@mail.ru>
2020-11-17 10:34:15 +0300

    add auth hook

--- src/client/hooks/auth.hook.js
+++ src/client/hooks/auth.hook.js
@@ -1,8 +1,33 @@
 import {useState, useEffect, useCallback, useMemo} from "react"
 
-const useAuth = () => {
-    
+const storageUser = 'userData'
 
+const useAuth = () => {
+    const [token, setToken] = useState('')
+
+    const login = useCallback((userToken) => {
+        setToken(userToken)
+
+        localStorage.setItem(storageUser, userToken)
+
+    }, [])
+
+    const logout = useCallback((userToken) => {
+        setToken(null)
+
+        localStorage.removeItem(storageUser)
+    }, [])
+
+    useEffect(() => {
+        const localStorageData = localStorage.getItem(storageUser)
+        console.log('---------------------')
+        console.log(localStorageData)
+        if (localStorageData) {
+            login(localStorageData)
+        }
+    }, [])
+
+    return {login, logout, token}
 }
 
 export default useAuth
