7ee7df7948855efe4faf8a51d90461c97cbf4aef
Grisha Tkach <grisatkac@mail.ru>
2020-12-02 22:37:27 +0300

    add redux and router-dom

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,32 +1,32 @@
 import React, { useContext } from 'react'
+import {Route, Switch, NavLink} from 'react-router-dom'
 import AuthContext from '../context/authContext'
 
 const Header = () => {
     const {token, logout} = useContext(AuthContext)
 
-    const clickHandler = (e) => {
+    const clickLogOut = (e) => {
         e.preventDefault()
         logout(token)
     }
 
     return (
-        <header>
-            <div className="">Logo</div>
-
-            <nav>
+        <header className="header">
+        <div className="container">
+            <div className="header__logo"><span>PISTAPOOL</span></div>
+            <nav className="header__nav">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Team</li>
-                    <li>Contact</li>
+                    <NavLink to="/home"><li>Home</li></NavLink>
+                    <NavLink to="/about"><li>About</li></NavLink>
+                    <NavLink to="contact"><li>Contact</li></NavLink>
                 </ul>
             </nav>
-
-            <div className="">
-                <div>burger</div>
-                <button onClick={clickHandler}>Logout</button>
+            <div className="header__burger">
+                <span></span>
             </div>
-        </header>
+            <button onClick={clickLogOut}>Выйти</button>
+        </div>
+    </header>
     )
 }
 
