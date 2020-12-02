677ee1ba22a11c852b31af513d1314224a2eabb0
Grisha Tkach <grisatkac@mail.ru>
2020-12-02 22:39:33 +0300

    add todoinput component with redux logic

---
+++ src/client/components/ToDo/ToDoInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createTask } from '../../redux/actions/toDoTasks'
+
+const ToDoInput = () => {
+    const [value, setValue] = useState({title: ''})
+    const dispatch = useDispatch()
+    const changeInputHanler = (e) => {
+        e.persist()
+        setValue(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const onclickHandler = () => {
+        console.log(value.title)
+        dispatch(createTask(value))
+        setValue({title: ''})
+    }
+    return (
+        <div>
+            <label htmlFor="input">Заголовок задачи</label>
+            <input 
+                type="text"
+                id="input"
+                name="title"
+                value={value.title}
+                onChange={changeInputHanler}
+            />
+            <button onClick={onclickHandler}>Создать задачу</button>
+        </div>
+    )
+}
+
+export default ToDoInput
