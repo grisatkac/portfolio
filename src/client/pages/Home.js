b53ed1ba56ed53205956a45276cac2523b38a21f
Grisha Tkach <grisatkac@mail.ru>
2020-12-02 22:40:06 +0300

    add basic pages

---
+++ src/client/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ToDo from './ToDo'
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                Home page
+            </div>
+            <div>
+                To do app
+                <ToDo />
+            </div>
+        </>
+    )
+}
+
+export default Home
