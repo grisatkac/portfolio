0be0dd573e0c72d59ce0ea2c68ab8b9deb368ab0
Grisha Tkach <grisatkac@mail.ru>
2020-12-02 22:40:14 +0300

    add todo page

---
+++ src/client/pages/ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ToDoInput from '../components/ToDo/ToDoInput'
+import ToDoTasks from '../components/ToDo/ToDoTasks'
+
+
+const ToDo = () => {
+    
+    return (
+        <div>
+            <ToDoInput />
+            <ToDoTasks />
+        </div>
+    )
+}
+
+export default ToDo
