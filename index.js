baca07d1ca17e011ca4f98c1926578217ac56ee3
Grisha Tkach <grisatkac@mail.ru>
2021-01-02 14:26:58 +0300

    change CORS politicy

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express()
 
 app.use(express.json({extented: true}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
+    next();
+  });
+  
+
 app.use('/api/auth', require('./src/server/routes/auth.routes'))
 
 async function start() {
