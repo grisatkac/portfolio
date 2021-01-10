3c38eeec903ad1f2a67041aaf5c18aca9faed939
Grisha Tkach <grisatkac@mail.ru>
2021-01-11 01:15:26 +0300

    del use selector

--- src/client/components/ToDo/ToDoTasks.js
+++ src/client/components/ToDo/ToDoTasks.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import Task from './Task'
+import TaskItem from './TaskItem'
 
-const ToDoTasks = () => {
-    const tasks = useSelector(state => state.ToDoList.tasks)
-    
+
+const ToDoTasks = ({ tasks, activeFilter }) => {
+    console.log('todotasks')
     if (!tasks.length) {
         return (
             <div>Список задач пуст. Добавьте задачи!</div>
         )
     }
-    console.log(tasks)
-    return tasks.map(task => <Task task={task.title} /> )
+    
+    return (
+        <ul>
+            {tasks.map(task => <TaskItem task={task.title} key={task.title}/>)}
+        </ul>)
 }
 
 export default ToDoTasks
