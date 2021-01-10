398b832a42a9cefecd7a22d3ca82fdff38e45af5
Grisha Tkach <grisatkac@mail.ru>
2021-01-11 01:17:11 +0300

    add case TODO/CHANGE_FILTER

--- src/client/redux/reducers/toDoReducer.js
+++ src/client/redux/reducers/toDoReducer.js
@@ -1,10 +1,14 @@
 const initialState = {
-    tasks: []
+    tasks: [],
+    filter: 'all'
 }
 export const toDoReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'TODO/CREATE_TASK':
-            return {...state, tasks: state.tasks.concat([action.payload]) }
+            return { ...state, tasks: state.tasks.concat([action.payload]) }
+        case 'TODO/CHANGE_FILTER':
+            return { ...state, filter: payload }
+
         default:
             return state;
     }
