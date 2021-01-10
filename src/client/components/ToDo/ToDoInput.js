29857c240dabff187752ce61985703cc521368a7
Grisha Tkach <grisatkac@mail.ru>
2021-01-11 01:14:43 +0300

    add memo

--- src/client/components/ToDo/ToDoInput.js
+++ src/client/components/ToDo/ToDoInput.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createTask } from '../../redux/actions/toDoTasks'
 
 const ToDoInput = () => {
     const [value, setValue] = useState({title: ''})
     const dispatch = useDispatch()
+
     const changeInputHanler = (e) => {
         e.persist()
         setValue(prevState => ({
@@ -12,11 +13,13 @@ const ToDoInput = () => {
             [e.target.name]: e.target.value
         }))
     }
+
     const onclickHandler = () => {
         console.log(value.title)
         dispatch(createTask(value))
         setValue({title: ''})
     }
+
     return (
         <div>
             <label htmlFor="input">Заголовок задачи</label>
@@ -27,9 +30,10 @@ const ToDoInput = () => {
                 value={value.title}
                 onChange={changeInputHanler}
             />
+            
             <button onClick={onclickHandler}>Создать задачу</button>
         </div>
     )
 }
 
-export default ToDoInput
+export default React.memo(ToDoInput)
