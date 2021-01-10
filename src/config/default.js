fad7b233acea6c136f9ff95c06e8985abc68a0f3
Grisha Tkach <grisatkac@mail.ru>
2021-01-11 01:16:42 +0300

    add field ToDoFilters

--- src/config/default.js
+++ src/config/default.js
@@ -1,5 +1,6 @@
 export default {
-    'mongoURL': 'mongodb+srv://Grisha:123456Aa@cluster0.f4mbl.azure.mongodb.net/portfolio?retryWrites=true&w=majority'
+    'mongoURL': 'mongodb+srv://Grisha:123456Aa@cluster0.f4mbl.azure.mongodb.net/portfolio?retryWrites=true&w=majority',
+    'ToDoFilters': ['all', 'active', 'completed']
 }
 
 //'mongodb+srv://Grisha:<password>@cluster0.f4mbl.azure.mongodb.net/<dbname>?retryWrites=true&w=majority'
