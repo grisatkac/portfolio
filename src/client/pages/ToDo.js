f742c14ea72bfc2a4fc1a88f58672cdf4996356b
Grisha Tkach <grisatkac@mail.ru>
2021-01-11 01:16:25 +0300

    add state from redux and method for toDoFooter.js

--- src/client/pages/ToDo.js
+++ src/client/pages/ToDo.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import ToDoFooter from '../components/ToDo/ToDoFooter'
 import ToDoInput from '../components/ToDo/ToDoInput'
 import ToDoTasks from '../components/ToDo/ToDoTasks'
 
 
 const ToDo = () => {
-    
+    const tasks = useSelector(state => state.ToDoList.tasks)
+    const [activeFilter, setActiveFilter] = useState('all')
+
     return (
-        <div>
+        <>
             <ToDoInput />
-            <ToDoTasks />
-        </div>
+            <ToDoTasks tasks={tasks} activeFilter={activeFilter}/>
+            <ToDoFooter tasks={tasks} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
+        </>
     )
 }
 
