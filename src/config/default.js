41ffe675c7c3873056f337df45247cd03a52586c
Grisha Tkach <grisatkac@mail.ru>
2021-01-15 19:08:50 +0300

    add field secret key

--- src/config/default.js
+++ src/config/default.js
@@ -1,6 +1,7 @@
 export default {
     'mongoURL': 'mongodb+srv://Grisha:123456Aa@cluster0.f4mbl.azure.mongodb.net/portfolio?retryWrites=true&w=majority',
-    'ToDoFilters': ['all', 'active', 'completed']
+    'ToDoFilters': ['all', 'active', 'completed'],
+    'secretKey': 'pistapool key'
 }
 
 //'mongodb+srv://Grisha:<password>@cluster0.f4mbl.azure.mongodb.net/<dbname>?retryWrites=true&w=majority'
