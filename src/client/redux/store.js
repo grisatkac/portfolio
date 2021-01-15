41eaa9a803fd23f3ae82b956ebed1b54f9333743
Grisha Tkach <grisatkac@mail.ru>
2021-01-15 19:18:43 +0300

    use in createStote composeWithDevTools

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,4 +1,6 @@
-const { createStore } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { composeWithDevTools } = require('redux-devtools-extension')
 const { rootReducer } = require("./reducers/rootReducer");
 
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()))
