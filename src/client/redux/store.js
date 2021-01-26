0dfa61693c6c5080d96605fbda147b57f6cd972b
Grisha Tkach <grisatkac@mail.ru>
2021-01-26 20:03:37 +0300

    add saga middleware in redux

--- src/client/redux/store.js
+++ src/client/redux/store.js
@@ -1,6 +1,14 @@
-const { createStore, applyMiddleware } = require("redux");
-const { composeWithDevTools } = require('redux-devtools-extension')
+//const { createStore, applyMiddleware } = require("redux");
+import {createStore, applyMiddleware} from 'redux'
+//const { composeWithDevTools } = require('redux-devtools-extension')
+import {composeWithDevTools} from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+import { updateToDoListWatcher } from './sagas';
 const { rootReducer } = require("./reducers/rootReducer");
 
+const sagaMiddleware = createSagaMiddleware()
+
 //export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(updateToDoListWatcher)
