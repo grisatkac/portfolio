f257c2fa75bdcf5ce3e3ee754622eaea355d1e89
Grisha Tkach <grisatkac@mail.ru>
2021-01-29 17:13:53 +0300

    add check cookie for the future

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 module.exports = ( async (req, res, next) => {
     try {
         const token = req.headers.authorization
-
+        //const token = req.headers.Cookie
         if (!token) {
             return res.status(401).json({message:'Нет авторизации'})
         }
