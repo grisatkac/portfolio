ea12646cf98b22e7e51460573e5780c79c086ac5
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:46:35 +0300

    change worker

--- src/client/redux/sagas.js
+++ src/client/redux/sagas.js
@@ -1,23 +1,53 @@
 import {takeEvery, put, call} from 'redux-saga/effects'
-import { createTask } from './actions/toDo'
+import { changeTask, createTask, deleteTask } from './actions/toDo'
 
 export function* updateToDoListWatcher() {
     yield takeEvery('TODO/UPDATE_LIST', updateToDoListWorker)
 }
 
-function* updateToDoListWorker({payload}) {
+function* updateToDoListWorker({payload: {data, action}}) {
     try {
-        const data = yield call(fetchUpdateToDoList)
+        console.log('---data---')
         console.log(data)
-        yield put({type: 'TODO/CREATE_TASK', payload})
+        console.log('---action---')
+        console.log(action)
+        switch (action) {
+            case 'TODO/CREATE_TASK':
+                //const data = yield call(() => fetchUpdateToDoList(payload))
+                yield call(() => {fetchUpdateToDoList(data, 'http://localhost:3000/app/todo/create')})
+                yield put(createTask(data))
+                break;
+            case 'TODO/DELETE_TASK':
+                console.log(data)
+                //yield call(() => {fetchUpdateToDoList({taskId: data}, 'http://localhost:3000/app/todo/delete')})
+                yield put(deleteTask(data))
+                break;
+            case 'TODO/CHANGE_TASK':
+                //yield call(() => {fetchUpdateToDoList(data, 'http://localhost:3000/app/todo/change')})
+                yield put(changeTask(data))
+                break;
+            default:
+                break;
+        }
+        /*const data = yield call(() => fetchUpdateToDoList(payload))
+        console.log(data)*/
+        //yield put({type: 'TODO/CREATE_TASK', payload})
+        /*console.log('---data---')
+        console.log(data)
+        console.log('---action---')
+        console.log(action)
+ 
+        yield put(createTask(data))*/
     } catch (error) {
         console.log('Error: ', error)
     }
 }
 
-const fetchUpdateToDoList = async () => {
-    const response = await fetch('http://localhost:3000/app/todo/update', {
+const fetchUpdateToDoList = async (body, url) => {
+    //const response = await fetch('http://localhost:3000/app/todo/update', {
+    const response = await fetch( url, {
         method: 'POST',
+        body: JSON.stringify(body),
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
