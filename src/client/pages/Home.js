e92cadd66298480aff1f3a21f2270c1d10a82a6f
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:50:07 +0300

    add some changes

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             </div>
             <div>
                 <ToDo />
-            </div>
+            </div>    
         </>
     )
 }
--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -9,11 +9,12 @@ module.exports = ( async (req, res, next) => {
             return res.status(401).json({message:'Нет авторизации'})
         }
 
-        const decoded = jwt.verify(token, 'pistapool key')
-
+        const decoded = await jwt.verify(token, 'pistapool key')
+        
         req.user = decoded
         next()
     } catch (error) {
+        console.log(error)
         res.status(401).json({message: 'Авторизация не выполнена'})
     }
 })
