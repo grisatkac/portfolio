39a943f9e78f7160049f5ba4ae151fdf9aa05bc4
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:48:14 +0300

    change ref Tasks to Task

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -3,7 +3,7 @@ const {Schema, model, Types} = require('mongoose')
 const schema = new Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    tasks: [{type: Types.ObjectId, ref: 'Tasks'}]
+    tasks: [{type: Types.ObjectId, ref: 'Task'}]
 })
 
 module.exports = model('User', schema)
