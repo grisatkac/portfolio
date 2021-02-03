a2b4dcf824ca9514f3b5e673fd700af1211eccc2
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:46:04 +0300

    add fetching method

--- src/client/pages/ToDo.js
+++ src/client/pages/ToDo.js
@@ -1,13 +1,15 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useCallback, useContext, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import ToDoFooter from '../components/ToDo/ToDoFooter'
 import ToDoInput from '../components/ToDo/ToDoInput'
 import ToDoTasks from '../components/ToDo/ToDoTasks'
 import Context from '../context/authContext'
 import useHttp from '../hooks/http.hook'
+import { createTask } from '../redux/actions/toDo'
 
 
 const ToDo = () => {
+    const dispatch = useDispatch()
     const {request} = useHttp()
     const {token} = useContext(Context)
     const tasks = useSelector(state => state.ToDoList.tasks)
@@ -16,23 +18,30 @@ const ToDo = () => {
     console.log('tasks tood', tasks)
 
     const fetchTasks = useCallback(async () => {
-        const response = await request('http://localhost:3000/app/todo/tasks', {
-            method: 'GET', body: null, headers: {authorization: token}}
-        )
-        console.log('---response---')
-        console.log(response)
+        try {
+            const data = await request('http://localhost:3000/app/todo/tasks', 'GET', null, {
+                'authorization': token
+            })
+            console.log(data)
+            dispatch(createTask(data.tasks))
 
-    }, [token])
-    
+        } catch (error) {
+            console.log(error)
+        }
+    }, [token, request])
 
-    
+    if (tasks) {
+       useEffect(() => {
+           console.log('tasks length')
+           fetchTasks()
+       }, [fetchTasks])
+    }
 
     return (
         <>
             <ToDoInput />
             <ToDoTasks tasks={tasks} activeFilter={activeFilter}/>
             <ToDoFooter tasks={tasks} activeFilter={activeFilter}  />
-            <button onClick={fetchTasks}>gEt data</button>
         </>
     )
 }
