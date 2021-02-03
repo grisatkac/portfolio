5eee8a234f600c3d242fbdf7c8652cf4c56be905
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:47:14 +0300

    add cases in toDoReducer

--- src/client/redux/reducers/toDoReducer.js
+++ src/client/redux/reducers/toDoReducer.js
@@ -5,7 +5,13 @@ const initialState = {
 export const toDoReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'TODO/CREATE_TASK':
-            return { ...state, tasks: state.tasks.concat([action.payload]) }
+            return { ...state, tasks: state.tasks.concat(...[action.payload]) }
+        case 'TODO/DELETE_TASK':
+            console.log('delete tasks is working')
+            
+            //return { ...state, tasks: state.tasks.}
+        case 'TODO/CHANGE_TASK':
+            break;
         case 'TODO/CHANGE_FILTER':
             return { ...state, filter: action.payload }
 
