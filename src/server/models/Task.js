a74cb98dbab9e775ba3ade436771f5163f5282b2
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:49:44 +0300

    change type field cimpleted to String

--- src/server/models/Task.js
+++ src/server/models/Task.js
@@ -2,8 +2,8 @@ const {Schema, model, Types} = require('mongoose')
 
 const schema = new Schema({
     title: {type: String, required: true},
-    completed: {type: Boolean, required: true},
+    completed: {type: String, required: true},
     owner: {type: Types.ObjectId, ref: 'User'}
 })
 
-module.exports = model('Tasks', schema)
+module.exports = model('Task', schema)
