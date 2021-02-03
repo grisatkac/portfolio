8cafbbdb5b6d2f5afd0e1c133fea3028d8fb4502
Grisha Tkach <grisatkac@mail.ru>
2021-02-03 21:48:55 +0300

    add change delete create routes for tasks route

--- src/server/routes/taskList.routes.js
+++ src/server/routes/taskList.routes.js
@@ -4,13 +4,14 @@ const auth = require('../middleware/index')
 const Task = require('../models/Task')
 
 
-// /app/todo/tasks
+// /app/todo
 
 router.get('/tasks', auth, async (req, res) => {
     console.log(req.user.userId)
-
-    const tasks = Task.find({owner: req.user.userId})
+    console.log('task request is woking')
     
+    const tasks = await Task.find({owner: req.user.userId})
+    console.log(tasks)
     if (tasks) {
         console.log('tasks are ok')
     }
@@ -18,8 +19,55 @@ router.get('/tasks', auth, async (req, res) => {
     res.status(201).json({tasks})
 })
 
-router.post('/update', auth, async (req, res) => {
-    return res.status(200).json({message: 'Запрос прошел успешно'})
+router.post('/create', auth, async (req, res) => {
+    try {
+        /*console.log('request body update list: ')
+        console.log(req.body)
+        
+        console.log('title: ', title)
+        console.log('completed: ', completed)*/
+
+        const { title, completed } = req.body
+        
+        const newTask = new Task({
+            title, completed, owner: req.user.userId
+        })
+
+        await newTask.save()
+
+        return res.status(200).json({ message: 'Запрос прошел успешно' })
+
+    } catch (error) {
+        console.log(error)
+        return status(400).json({message: 'Сохранение изменений в БД не выполнена'})
+    }
+})
+
+router.post('/delete', auth, async (res, req) => {
+    try {
+        const {taskId} = req.body.taskId
+
+        await Task.deleteOne({_id: taskId}, (err) => {
+            if (err) {
+                return res.status(400).json({message: 'Сохранение изменений в БД не выполнена'})
+            }
+        })
+
+        return res.status(200).json({message: 'Пост успешно удален из БД'})
+        
+    } catch (error) {
+        console.log(error)
+        return status(400).json({message: 'Сохранение измненений в БД не выполнена'})
+    }
+})
+
+router.post('/change', auth, (res, req) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return status(400).json({message: 'Сохранение измненений в БД не выполнена'})
+    }
 })
 
 module.exports = router
