539ea0f846be7c2db77aa1bb1b62786b0d293a09
Grisha Tkach <grisatkac@mail.ru>
2021-02-08 15:22:41 +0300

    add verifying token logic

--- src/client/hooks/auth.hook.js
+++ src/client/hooks/auth.hook.js
@@ -1,10 +1,12 @@
 import {useState, useEffect, useCallback, useMemo} from "react"
+import useHttp from "./http.hook"
 
 const storageUser = 'userData'
 
 const useAuth = () => {
     const [token, setToken] = useState('')
     const [ready, setReady] = useState(false)
+    const {request} = useHttp()
 
     const login = useCallback((userToken) => {
         setToken(userToken)
@@ -18,10 +20,34 @@ const useAuth = () => {
     }, [])
 
     useEffect(() => {
+        console.log('check token')
         const localStorageData = localStorage.getItem(storageUser)
-        if (localStorageData) {
+        console.log(localStorageData)
+        /*if (localStorageData) {
             login(localStorageData)
+        }*/
+
+        const verifyToken = async () => {
+            try {
+                const data = await request('http://localhost:3000/app/token/refresh', 'GET', null, {
+                    'authorization': localStorageData
+                })
+
+                if (data.message !== 'Авторизация не выполнена') {
+                    login(localStorageData)
+                } else {
+                    logout()
+                }
+
+            } catch (error) {
+                console.log(error)
+            }
         }
+
+        if (localStorageData) {
+            verifyToken()
+        }
+
         setReady(true)
     }, [login])
 
