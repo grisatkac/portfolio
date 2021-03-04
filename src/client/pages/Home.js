f670e60f6b5d448c0f83d8bba22f500776d0206e
Grisha Tkach <grisatkac@mail.ru>
2021-03-04 19:00:52 +0300

    add semantic in the Home.js

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -6,11 +6,72 @@ const Home = () => {
     return (
         <main className="main">
             <div className="main-container">
-                <div>
-                    <h1>Weclome to the PISTAPOOL</h1>
-                </div>
+                <section className="hero">
+                    <div className="hero-container">
+                        <div className='hero-text'>
+                            <p>Web development project</p>
+                            <p>Very suitable to support all web development projects</p>
+                        </div>
+                        
+                        <div className="hero-buttons">
+                            <button>OUR SERVICES</button>
+                            <button>HIRE IS NOW</button>
+                        </div>
+                    </div>
+                </section>
 
-                <div className="todo">
+                <section className="about">
+                    <div className="about-container">
+                        <div className="about-text">
+                            <h2>About Us</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className="about-button">
+
+                            <NavLink to='about'>
+                                <button>
+                                    Learn more
+                                </button>
+                            </NavLink>
+
+                        </div>
+                    </div>
+                </section>
+
+                <section className="portfolio">
+                    <div className="portfolio-container">
+                        <div className="portfolio-text">
+                            <h2>Portfolio</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className="portfolio-button">
+                            <NavLink to='/tasks'>
+                                <button>
+                                    Learn more
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="contact">
+                    <div className="contact-container">
+                        <div className="contact-text">
+                            <h2>Contact Us</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className="contact-button">
+
+                            <NavLink to='contact'>
+                                <button>
+                                    Learn more
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
+                </section>
+
+                {/*<div className="todo">
                     <div className="todo-container">
                         <div>Здесь вы можете можете составлять планы</div>
                     </div>
@@ -20,7 +81,7 @@ const Home = () => {
                 </div>
                 <div>
                     <NavLink to="/messages">Сообщения</NavLink>
-                </div>
+    </div>*/}
             </div>
         </main>
     )
