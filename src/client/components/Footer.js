5400ee29b028bb02dac73f805eefd3b11b2a99e1
Grisha Tkach <grisatkac@mail.ru>
2021-03-04 19:00:15 +0300

    add semantic in the footer

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>FOOTER</p>
+            <div className="footer-container">
+                <div className="footer-social">
+                    <ul className="social-list">
+                        <li className="social-link social-link-vk"><a href=""></a></li>
+                        <li className="social-link social-link-inst"><a href=""></a></li>
+                        <li className="social-link social-link-fac"><a href=""></a></li>
+                        <li className="social-link social-link-tw"><a href=""></a></li>
+                        <li className="social-link social-link-in"><a href=""></a></li>
+                    </ul>
+                </div>
+                <div className="footer-text">
+                    <p>Pistapool @ 2021</p>
+
+                </div>
+            </div>
         </footer>
     )
 }
