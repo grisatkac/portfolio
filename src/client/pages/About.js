117293a358d69a8e959a69b5a68718f0fe4aa082
Grisha Tkach <grisatkac@mail.ru>
2021-03-12 17:57:38 +0300

    add html in the about page

--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div className="main-container">
                 <div className="about-description">
                     <div className="description-header">
-                        <p>About Us</p>
+                        <h2>About Us</h2>
                         <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</p>
                     </div>
                     <div className="description-list">
@@ -44,31 +44,57 @@ const About = () => {
                 </div>
 
                 <div className="about-pricing">
-                    <div className="pricing-header"></div>
-                    <div className="pricing-offers">
-                        <div className="free-offer">
-                            <div className="offer-header">
-                                <p>Free Trail</p>
-                                <p>00 per month</p>
-                            </div>
-                            <div className="offer-description"></div>
-                            <div className="offer-button"></div>
+                    <div className="about-pricing-container">
+                        <div className="pricing-header">
+                            <h2>Pricing Plan</h2>
+                            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</p>
                         </div>
-                        <div className="basic-offer">
-                            <div className="offer-header">
-                                <p>Basic</p>
-                                <p>50 per month</p>
+                        <div className="pricing-offers">
+                            <div className="free-offer">
+                                <div className="offer-header">
+                                    <p>Free Trail</p>
+                                    <p>00 per month</p>
+                                </div>
+                                <div className="offer-description">
+                                    <p>30 Free Trail</p>
+                                    <p>5 Free Projects</p>
+                                    <p>PHP 5 Enabled</p>
+                                    <p>24/7 Supports</p>
+                                </div>
+                                <div className="offer-button">
+                                    <button>Order Now</button>
+                                </div>
                             </div>
-                            <div className="offer-description"></div>
-                            <div className="offer-button"></div>
-                        </div>
-                        <div className="ultimates-offer">
-                            <div className="offer-header">
-                                <p>Ultimates</p>
-                                <p>99 per month</p>
+                            <div className="basic-offer">
+                                <div className="offer-header">
+                                    <p>Basic</p>
+                                    <p>50 per month</p>
+                                </div>
+                                <div className="offer-description">
+                                    <p>30 Free Trail</p>
+                                    <p>5 Free Projects</p>
+                                    <p>PHP 5 Enabled</p>
+                                    <p>24/7 Supports</p>
+                                </div>
+                                <div className="offer-button">
+                                    <button>Order Now</button>
+                                </div>
                             </div>
-                            <div className="offer-description"></div>
-                            <div className="offer-button"></div>
+                            <div className="ultimates-offer">
+                                <div className="offer-header">
+                                    <p>Ultimates</p>
+                                    <p>99 per month</p>
+                                </div>
+                                <div className="offer-description">
+                                    <p>30 Free Trail</p>
+                                    <p>5 Free Projects</p>
+                                    <p>PHP 5 Enabled</p>
+                                    <p>24/7 Supports</p>
+                                </div>
+                                <div className="offer-button">
+                                    <button>Order Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
