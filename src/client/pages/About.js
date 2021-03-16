dcfc3ce45885acee462b7d91c43011a193dc95ec
Grisha Tkach <grisatkac@mail.ru>
2021-03-16 17:31:27 +0300

    add html and imgs in the about page

--- src/client/pages/About.js
+++ src/client/pages/About.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Internet from "../../public/img/internet.png"
 import Transfer from "../../public/img/transfer.png"
 import Head from "../../public/img/head.png"
+import Team1 from "../../public/img/team1.png"
+import Team2 from "../../public/img/team2.png"
+import Team3 from "../../public/img/team3.png"
 
 const About = () => {
     return (
@@ -100,7 +103,38 @@ const About = () => {
                 </div>
 
                 <div className="about-team">
-
+                    <div className="about-team-container">
+                        <div className="team-header">
+                            <h2>Our Team Member</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                        </div>
+                        <div className="team-list">
+                            <div className="employee">
+                                <div className="employee-photo">
+                                    <img src={Team1}></img>
+                                </div>
+                                <div className="employee-name">John Doe </div>
+                                <div className="employee-position">General Manager</div>
+                                <div className="employee-social-links"></div>
+                            </div>
+                            <div className="employee">
+                                <div className="employee-photo">
+                                    <img src={Team2}></img>
+                                </div>
+                                <div className="employee-name">Alexa Biru</div>
+                                <div className="employee-position">Office Director</div>
+                                <div className="employee-social-links"></div>
+                            </div>
+                            <div className="employee">
+                                <div className="employee-photo">
+                                    <img src={Team3}></img>
+                                </div>
+                                <div className="employee-name">Ronobir sing</div>
+                                <div className="employee-position">Admin</div>
+                                <div className="employee-social-links"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
