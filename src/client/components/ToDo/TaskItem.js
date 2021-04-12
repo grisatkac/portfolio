7c41ff703055b331dd5de26e2f66cd0d34c3bd2d
Grisha Tkach <grisatkac@mail.ru>
2021-04-12 15:32:40 +0300

    add icons and logic for changing status of complete

--- src/client/components/ToDo/TaskItem.js
+++ src/client/components/ToDo/TaskItem.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { updateToDoList } from '../../redux/actions/toDo'
 
-const TaskItem = ({ task }) => {
-    const dipsatch = useDispatch()
-    
-    const onclickHandler = () => {
-        dipsatch(updateToDoList(task._id, 'TODO/DELETE_TASK'))
+const TaskItem = ({task: { _id, title, completed }}) => {
+    //const [isCompleted, setCompleted] = useState(completed)
+    const dispatch = useDispatch()
+    console.log(title, completed, _id)
+    const deleteHandler = () => {
+        dispatch(updateToDoList(_id, 'TODO/DELETE_TASK'))
+    }
+
+    const changeStatusOfComplete = () => {
+        const isCompleted = (completed === 'active') ? 'completed' : 'active';
+        
+        dispatch(updateToDoList({_id, completed: isCompleted} , 'TODO/CHANGE_TASK'))
     }
     
     return (
         <li>
-            <p>{task.title}</p>
-            <button onClick={onclickHandler}>Удалить</button>
+             <i 
+                className={ (completed === 'active') ? 'mark far fa-circle' : 'mark far fa-check-circle'} 
+                onClick={changeStatusOfComplete}
+            />
+             {/*<i  className={isCompleted ? 'mark far fa-check-circle' : 'mark far fa-circle'} />*/}
+            <p>{title}</p>
+            <button 
+                onClick={deleteHandler}
+                value={'deleteBtn'}>Удалить</button>
+            <i className="fas fa-trash-alt" 
+               onClick={deleteHandler}
+            />
         </li>
     )
 }
